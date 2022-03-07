8f9412325af34543c033694935af9ce20e36616b
Hoo <2501798@gmail.com>
2022-03-07 20:47:26 +0900

    돔이용 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log('하이')
+const id = document.querySelector("#id"),
+ psword = document.querySelector("#psword"),
+ loginBtn = document.querySelector("button");
+
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+const req = {
+    id: id.value,
+    psword: psword.value,
+}
+}
+
+console.log(id);

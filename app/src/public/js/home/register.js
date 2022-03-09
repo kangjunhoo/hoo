32d6c82f5bffe82b1b88bcacb4984dfba073d407
Hoo <2501798@gmail.com>
2022-03-09 13:58:07 +0900

    24번째 회원가입

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,23 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+ name  = document.querySelector("#name"),
  psword = document.querySelector("#psword"),
- loginBtn = document.querySelector("#button");
+ confirmPsword = document.querySelector("#confirm-psword"),
+ registerBtn = document.querySelector("#button");
 
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login(){
+function register(){
 const req = {
     id: id.value,
+    name: name.value,
     psword: psword.value,
+    confirmPsword: confirmPsword.value,
 };
-fetch("/login",{
+console.log(req)
+fetch("/register",{
     method: "POST",
     headers:{
         "Content-Type": "application/json"
@@ -21,14 +26,14 @@ fetch("/login",{
 }).then((res)=> res.json())
   .then((res)=> {
       if(res.success){
-       location.href="/";
+       location.href="/login";
        }else{
            alert(res.msg);
        }
   
 })
 .catch((err)=>{
-    console.error("로그인중 에러 발생");
+    console.error("회원가입중 에러 발생");
 }) 
 
 }

c9858d30456f98ea0c11cf734195b8c6e49b4bb5
Hoo <2501798@gmail.com>
2022-03-10 14:58:42 +0900

    38번 오브젝트오류중

--- app/app.js
+++ app/app.js
@@ -4,11 +4,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config();
+
 const app = express();
+
+
+const accessLogStream = require("./src/config/log");
 
 // 라우팅
 const home = require("./src/routes/home");
+
+
+
+
 
 // 앱세팅
 app.set("views","./src/views");
@@ -16,6 +25,8 @@ app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(morgan("dev"));
+app.use(morgan("common",{stream: accessLogStream}));
 
 app.use("/",home);
 
